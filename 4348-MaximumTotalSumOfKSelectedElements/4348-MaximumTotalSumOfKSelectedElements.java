b862ed35aa5189faeb610d419517a0b55ddd6f3a
saran25685-droid <saran25685@gmail.com>
2026-08-13 09:21:23 +0530

    Create 4348-MaximumTotalSumOfKSelectedElements

---
+++ 4348-MaximumTotalSumOfKSelectedElements/4348-MaximumTotalSumOfKSelectedElements.java
@@ -0,0 +1,17 @@
+// Last updated: 13/08/2026, 09:21:23
+class Solution {
+    public long maxSum(int[] nums, int k, int mul) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        long sum=0;
+    
+        for(int i=n-1;i>=n-k;i--){
+            if(mul>0){
+            sum+=(long)nums[i]*mul;
+            mul--;}else{
+                sum+=nums[i];
+            }
+        }
+        return sum;
+    }
+}
