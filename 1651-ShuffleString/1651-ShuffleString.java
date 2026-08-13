a04c51d31f7711fe52b5a72b0db53f9ebcfd0496
saran25685-droid <saran25685@gmail.com>
2026-08-13 09:23:39 +0530

    Create 1651-ShuffleString

---
+++ 1651-ShuffleString/1651-ShuffleString.java
@@ -0,0 +1,16 @@
+// Last updated: 13/08/2026, 09:23:40
+class Solution {
+    public String restoreString(String s, int[] indices) {
+          int length=s.length();
+        StringBuilder sb=new StringBuilder("");
+         char c[]=new char[length];
+       
+        for(int i=0;i<length;i++){
+
+            c[indices[i]]=s.charAt(i);
+
+        }
+        sb.append(c);
+        return sb.toString();
+    }
+}
