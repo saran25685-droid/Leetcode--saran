a40e2747e7bad345b75d660c68199ea341278e2e
saran25685-droid <saran25685@gmail.com>
2026-08-13 09:35:20 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.java
@@ -0,0 +1,19 @@
+// Last updated: 13/08/2026, 09:35:20
+class RecentCounter {
+    private static final int[] records = new int[10000]; //
+    private int start;
+    private int end;
+
+    public RecentCounter() {        
+        start = 0;
+        end = 0;
+    }
+    
+    public int ping(int t) {
+        while (start < end && (t - records[start] > 3000)) { 
+            start++; 
+        }
+        records[end++] = t; 
+        return end - start; 
+    }
+}
