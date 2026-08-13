3a87045d63ee1ea5ac005bbb1f719ca8bd560341
saran25685-droid <saran25685@gmail.com>
2026-08-13 09:37:50 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,17 @@
+// Last updated: 13/08/2026, 09:37:50
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int count_v = 0, count_h = 0;
+
+        for (int i = 0; i < moves.length(); i++) {
+            char ch = moves.charAt(i);
+
+            if (ch == 'U') count_v++;
+            else if (ch == 'D') count_v--;
+            else if (ch == 'R') count_h++;
+            else count_h--;
+        }
+
+        return count_v == 0 && count_h == 0;
+    }
+}
